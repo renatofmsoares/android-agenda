4c35d26635038d99a468bd30a51c358da7049453
Renato Soares <tqi_rsoares@uolinc.com>
2020-07-08 14:56:22 -0300

    first commit

---
+++ app/src/main/java/alura/com/br/MainActivity.java
@@ -0,0 +1,17 @@
+package alura.com.br;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Toast.makeText(this, "Renato Soares", Toast.LENGTH_LONG).show();
+    }
+}
