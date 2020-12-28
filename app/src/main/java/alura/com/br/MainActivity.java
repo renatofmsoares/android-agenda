8aaabf3a6f50288902a3fde9689d54aacfec652e
Renato Soares <renatof.m.soares@gmail.com>
2020-12-28 20:34:22 -0300

    Aplicando layout para a Activity

--- app/src/main/java/alura/com/br/MainActivity.java
+++ app/src/main/java/alura/com/br/MainActivity.java
@@ -2,16 +2,34 @@ package alura.com.br;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView((R.layout.activity_main));
+        List<String> alunos = new ArrayList<>(
+                Arrays.asList("Aline", "Rodrigo", "Renato")
+        );
 
-        Toast.makeText(this, "Renato Soares", Toast.LENGTH_LONG).show();
+        ListView list_view_alunos = findViewById(R.id.activity_main_lista_de_alunos);
+        list_view_alunos.setAdapter(new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                alunos
+        ));
+
+
+
     }
 }
